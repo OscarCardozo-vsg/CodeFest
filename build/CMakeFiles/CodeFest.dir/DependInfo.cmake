a371c2847731546c2e7d1f7191d03281ab522c80
Oscar_Cardozo <oscar.cardozo30@hotmail.com>
2024-07-30 12:22:23 -0500

    CmakeList Upgraded
    Se cambio las rutas de las librerias en el CmakeLists

---
+++ build/CMakeFiles/CodeFest.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/toto/Documentos/GitHub/CODEFEST-AD-ASTRA/CodeFest/decrypt.cpp" "CMakeFiles/CodeFest.dir/decrypt.cpp.o" "gcc" "CMakeFiles/CodeFest.dir/decrypt.cpp.o.d"
+  "/home/toto/Documentos/GitHub/CODEFEST-AD-ASTRA/CodeFest/encrypt.cpp" "CMakeFiles/CodeFest.dir/encrypt.cpp.o" "gcc" "CMakeFiles/CodeFest.dir/encrypt.cpp.o.d"
+  "/home/toto/Documentos/GitHub/CODEFEST-AD-ASTRA/CodeFest/main.cpp" "CMakeFiles/CodeFest.dir/main.cpp.o" "gcc" "CMakeFiles/CodeFest.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CodeFest.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeFest.dir/decrypt.cpp.o"
+  "CMakeFiles/CodeFest.dir/decrypt.cpp.o.d"
+  "CMakeFiles/CodeFest.dir/encrypt.cpp.o"
+  "CMakeFiles/CodeFest.dir/encrypt.cpp.o.d"
+  "CMakeFiles/CodeFest.dir/main.cpp.o"
+  "CMakeFiles/CodeFest.dir/main.cpp.o.d"
+  "CodeFest"
+  "CodeFest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeFest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
